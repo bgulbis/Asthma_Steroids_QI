dba7c0396ad54c87bc7752f76232eedb9e07c208
bgulbis <brian.gulbis@gmail.com>
2016-04-25 16:33:53 -0500

    exclude pregnant patients

--- 2-exclude.R
+++ 2-exclude.R
@@ -10,3 +10,13 @@ raw.persons <- read_edw_data(dir.patients, "demographics") %>%
     distinct
 
 concat_encounters(raw.persons$person.id)
+
+# exclude any pregnant patients
+
+preg.icd9 <- read_edw_data(dir.patients, "icd9")
+preg.icd10 <- read_edw_data(dir.patients, "icd10")
+preg.labs <- read_edw_data(dir.patients, "labs_preg", "labs")
+
+excl.preg <- check_pregnant(preg.labs, preg.icd9, preg.icd10)
+
+patients$exclude_pregnant = excl.preg$pie.id
