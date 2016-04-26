dbb50d6f59fd57a0d781e2ed1268c267412042a1
bgulbis <brian.gulbis@gmail.com>
2016-04-26 11:03:18 -0500

    exclude icu patients

--- 2-exclude.R
+++ 2-exclude.R
@@ -4,6 +4,8 @@ source("0-library.R")
 
 tmp <- get_rds(dir.save)
 
+include <- data_frame(pie.id = patients$eligible)
+
 # get list of unique person id's to use for identifying re-encounters
 raw.persons <- read_edw_data(dir.patients, "demographics") %>%
     select(person.id) %>%
@@ -11,6 +13,7 @@ raw.persons <- read_edw_data(dir.patients, "demographics") %>%
 
 concat_encounters(raw.persons$person.id)
 
+# pregnant ----
 # exclude any pregnant patients
 
 preg.icd9 <- read_edw_data(dir.patients, "icd9")
@@ -20,3 +23,27 @@ preg.labs <- read_edw_data(dir.patients, "labs_preg", "labs")
 excl.preg <- check_pregnant(preg.labs, preg.icd9, preg.icd10)
 
 patients$exclude_pregnant = excl.preg$pie.id
+
+include <- anti_join(include, excl.preg, by = "pie.id")
+
+# icu admission ----
+# exclude patients who were first admitted to the PICU
+
+picu <- "Hermann 9 Pediatric Intensive Care Unit"
+ed <- "HC Virtual Pedi Emergency Dept"
+
+locations <- read_edw_data(dir.patients, "locations") %>%
+    tidy_data("locations")
+
+excl.location <- locations %>%
+    filter((unit.count == 1 & location == picu) |
+               (unit.count == 2 & location == picu & lag(location) == ed)) %>%
+    distinct(pie.id)
+
+patients$exclude_icu = excl.location$pie.id
+
+include <- anti_join(include, excl.location, by = "pie.id")
+
+# both steroids ----
+
+save_rds(dir.save, "^patients")
