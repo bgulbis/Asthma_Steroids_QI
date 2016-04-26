80d12ab9e819d616b7bb1b1cb6cfdbf329fe448f
bgulbis <brian.gulbis@gmail.com>
2016-04-26 11:30:37 -0500

    exclude patients receiving multiple steroids or racemic epi

--- 2-exclude.R
+++ 2-exclude.R
@@ -16,9 +16,12 @@ concat_encounters(raw.persons$person.id)
 # pregnant ----
 # exclude any pregnant patients
 
-preg.icd9 <- read_edw_data(dir.patients, "icd9")
-preg.icd10 <- read_edw_data(dir.patients, "icd10")
-preg.labs <- read_edw_data(dir.patients, "labs_preg", "labs")
+preg.icd9 <- read_edw_data(dir.patients, "icd9") %>%
+    semi_join(include, by = "pie.id")
+preg.icd10 <- read_edw_data(dir.patients, "icd10") %>%
+    semi_join(include, by = "pie.id")
+preg.labs <- read_edw_data(dir.patients, "labs_preg", "labs") %>%
+    semi_join(include, by = "pie.id")
 
 excl.preg <- check_pregnant(preg.labs, preg.icd9, preg.icd10)
 
@@ -45,5 +48,35 @@ patients$exclude_icu = excl.location$pie.id
 include <- anti_join(include, excl.location, by = "pie.id")
 
 # both steroids ----
+# exclude patients who received both dexamethasone and prednisone/prednisolone
+
+excl.steroids <- read_edw_data(dir.patients, "meds_freq", "meds_sched_freq") %>%
+    semi_join(include, by = "pie.id") %>%
+    distinct(pie.id, med) %>%
+    mutate(med = factor(med),
+           value = TRUE) %>%
+    group_by(pie.id) %>%
+    select(pie.id, med, value) %>%
+    spread(med, value) %>%
+    filter(dexamethasone == TRUE,
+           (prednisone == TRUE | prednisolone == TRUE))
+
+patients$exclude_multiple_steroids = excl.steroids$pie.id
+
+include <- anti_join(include, excl.steroids, by = "pie.id")
+
+# racemic epi ----
+
+excl.racepi <- read_edw_data(dir.patients, "meds_sched") %>%
+    semi_join(include, by = "pie.id") %>%
+    distinct(pie.id)
+
+patients$exclude_racemic_epi = excl.racepi$pie.id
+
+include <- anti_join(include, excl.racepi, by = "pie.id")
+
+# included patients ----
+
+patients$include = include$pie.id
 
 save_rds(dir.save, "^patients")
