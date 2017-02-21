a5aa0c321e9e575025ad6dacf81b3dff3ffa6bb2
bgulbis <brian.gulbis@gmail.com>
2017-02-21 11:49:31 -0600

    update script to make analysis tables using MBO data

--- src/4-analyze.R
+++ src/4-analyze.R
@@ -4,21 +4,21 @@ library(tidyverse)
 
 tmp <- dirr::get_rds("data/tidy")
 
-analyze_patients <- select(data_demographics, pie.id, age, sex, length.stay) %>%
-    inner_join(data_groups, by = "pie.id") %>%
-    inner_join(data_measures, by = "pie.id") %>%
-    inner_join(data_primary_diagnosis[c("pie.id", "diag.code", "icd.description")],
-               by = "pie.id") %>%
-    inner_join(data_readmit, by = "pie.id") %>%
-    inner_join(data_picu, by = "pie.id") %>%
-    inner_join(data_emesis, by = "pie.id")
+analyze_patients <- select(data_demographics, millennium.id, age, gender, length.stay) %>%
+    inner_join(data_groups, by = "millennium.id") %>%
+    inner_join(data_measures, by = "millennium.id") %>%
+    inner_join(data_primary_diagnosis[c("millennium.id", "diag.code", "icd.description")],
+               by = "millennium.id") %>%
+    inner_join(data_readmit, by = "millennium.id") %>%
+    inner_join(data_picu, by = "millennium.id") %>%
+    inner_join(data_emesis, by = "millennium.id")
 
-analyze_scores <- inner_join(data_groups, data_asthma, by = "pie.id")
+analyze_scores <- inner_join(data_groups, data_asthma, by = "millennium.id")
 
 analyze_steroids <- select(data_steroids, -first.datetime, -last.datetime) %>%
-    inner_join(data_groups, by = "pie.id")
+    inner_join(data_groups, by = "millennium.id")
 
-analyze_meds_adjunct <- inner_join(data_groups, data_meds_adjunct, by = "pie.id")
+analyze_meds_adjunct <- inner_join(data_groups, data_meds_adjunct, by = "millennium.id")
 
 analyze_identifiers <- select(data_identifiers, -person.id)
 
