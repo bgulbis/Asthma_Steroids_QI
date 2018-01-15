eedf56628c6f30754a17d314ef5e1928ddc27a1f
bgulbis <brian.gulbis@gmail.com>
2018-01-15 15:38:27 -0600

    finish getting data

--- src/01_get-patients.R
+++ src/01_get-patients.R
@@ -37,16 +37,23 @@ meds_albuterol <- raw_meds %>%
            (med.datetime >= mdy("9/22/2016", tz = "US/Central") &
                 med.datetime <= mdy("10/19/2016", tz = "US/Central")) |
                (med.datetime >= mdy("9/22/2017", tz = "US/Central") &
-                    med.datetime <= mdy("10/19/2017", tz = "US/Central")))
+                    med.datetime <= mdy("10/19/2017", tz = "US/Central"))) %>%
+    mutate(orig.order.id = if_else(order.parent.id == 0, order.id, order.parent.id))
 
 mbo_alb <- concat_encounters(unique(meds_albuterol$millennium.id))
 
+mbo_order <- concat_encounters(unique(meds_albuterol$orig.order.id), 1000)
+
 # step 3 -----------------------------------------------
-# run the following MBO queries:
+# run the following MBO queries using mbo_alb
 #   * Demographics
 #   * Diagnosis Codes (ICD-9/10-CM) - All
 #   * Measures
 #   * Vitals - BP
+
+# run the following MBO query using mbo_order
+#   * Orders Meds - Details - by Order id
+#   * Orders Meds - Details - by Order id, Volume
 
 # find ashtma patients; ICD 10 = J45.*
 
@@ -66,24 +73,67 @@ raw_demographics <- read_data(dir_raw, "demographics", FALSE) %>%
 raw_measures <- read_data(dir_raw, "measures", FALSE) %>%
     as.events(order_var = FALSE)
 
+raw_vitals <- read_data(dir_raw, "vitals", FALSE) %>%
+    as.vitals()
+
 pts_all <- raw_demographics %>%
     left_join(pts_asthma, by = "millennium.id")
 
 # step 4 -----------------------------------------------
+
+raw_orders <- read_data(dir_raw, "orders-details_", FALSE) %>%
+    as.order_detail()
+
+raw_orders_vol <- read_data(dir_raw, "orders-details-vol", FALSE) %>%
+    rename(`Order Strength Dose` = `Order Volume Dose`,
+           `Order Strength Dose Unit` = `Order Volume Dose Unit`) %>%
+    as.order_detail()
+    # anti_join(raw_orders, c("millennium.id", "order.id"))
+
+orders <- bind_rows(raw_orders, raw_orders_vol) %>%
+    arrange(millennium.id, order.id, order.datetime) %>%
+    filter(ingredient.unit %in% c("mg", "puff")) %>%
+    select(millennium.id, order.id, order.datetime, freq, prn) %>%
+    distinct()
+
+meds <- meds_albuterol %>%
+    left_join(orders, by = c("millennium.id", "orig.order.id" = "order.id"))
+
+meds_albuterol_cont <- meds %>%
+    filter(is.na(order.datetime))
+    # filter(!is.na(event.tag) | route == "IV")
+
+mbo_cont <- concat_encounters(unique(meds_albuterol_cont$orig.order.id), 1000)
+
+raw_orders_cont <- read_data(dir_raw, "orders-details-cont", FALSE) %>%
+    rename(`Order Strength Dose` = `Order Volume Dose`,
+           `Order Strength Dose Unit` = `Order Volume Dose Unit`) %>%
+    mutate(Frequency = NA_character_,
+           `PRN Indicator` = NA_character_) %>%
+    as.order_detail(extras = list(clinical.display = "`Complete Clinical Display Line`"))
+
+
+# run MBO query
+#   * Orders Meds - Details - by Order Id, Cont
+
+meds_albuterol_mdi <- meds %>%
+    filter(med.dose.units == "puff")
+
+
 # run the following queries:
 #   * Encounters - by Person ID
 
-raw_meds_sched <- read_data(dir_raw, "meds_sched") %>%
-    as.meds_sched()
-
-ref <- tibble(name = "albuterol", type = "med", group = "cont")
-
-raw_albuterol <- read_data(dir_raw, "meds_cont_asthma") %>%
-    as.meds_cont() %>%
-    tidy_data(ref, raw_meds_sched) %>%
-    distinct(order.id)
-
-edw_order <- concat_encounters(raw_albuterol$order.id)
+# raw_meds_sched <- read_data(dir_raw, "meds_sched") %>%
+#     as.meds_sched()
+#
+# ref <- tibble(name = "albuterol", type = "med", group = "cont")
+#
+# raw_albuterol <- read_data(dir_raw, "meds_cont_asthma") %>%
+#     as.meds_cont() %>%
+#     tidy_data(ref, raw_meds_sched) %>%
+#     distinct(order.id)
+#
+# edw_order <- concat_encounters(raw_albuterol$order.id)
 
 # step 5 -----------------------------------------------
 # run the following queries:
